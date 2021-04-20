6ee67461123d8391613bddf1da85ebb55f75e588
Austin Schroeder <austin@Andrews-iPhone.local>
2021-04-20 10:34:41 -0700

    Off to a great start.  Button adds divs, and A L keys make cars drive.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,49 @@
-console.log('hello dave')
+// When button is clicked:
+//      -2 race cars appear on left side of screen
+//      -instructions <p> appear on how to drive cars
+//
+//Tap A to drive car 1 (move to the right)
+//Tap L to drive car 2 (move to the right)
+
+
+const $button = $('button');
+const $racer1 = $(`<div class="one" />`);
+const $racer2 = $(`<div class="two" />`);
+const $instruction1 = $(`<p class="instruct1">Racer 1-Drive by smashing "A"</p>`);
+const $instruction2 = $(`<p class="instruct2">Racer 2-Drive by smashing "L"</p>`);
+
+
+$('button').on('click', () => {
+    $('.instructions').append($instruction1);
+    $('.instructions').append($instruction2);
+
+    $('.race-cars').append($racer1);
+    $('.race-cars').append($racer2);
+
+});
+
+// RACING LOGIC
+// keycode 65	"A"	
+// keycode 76	"L"
+
+let winnerOne = 0;
+let winnerTwo = 0;
+$(window).on("keydown", event => {
+    if (event.keyCode === 65) {
+       winnerOne += 1;
+       console.log(winner);
+
+        
+        $(".one").animate({ 
+            marginLeft: "+=10px",
+        }, 10 );
+
+    } else if (event.keyCode === 76) {
+        winnerTwo += 1;
+       console.log(winner);
+        $(".two").animate({ 
+            marginLeft: "+=10px",
+        }, 10 );
+        
+    }
+})
