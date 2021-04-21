2f0e10f89cfca7b3da91006d9d6970faa7ed95de
Austin Schroeder <austin@Andrews-iPhone.local>
2021-04-20 21:52:26 -0700

    Started adding boundingClientRect.  everything is weird now

--- scripts/app.js
+++ scripts/app.js
@@ -8,8 +8,8 @@
 
 
 const $button = $('button');
-const $racer1 = $(`<img class="one" src="https://i.pinimg.com/originals/eb/39/35/eb3935ae7c66765699a16865187bcd6c.png" />`);
-const $racer2 = $(`<img class="two" src="https://i.pinimg.com/originals/05/c0/68/05c068556d91081d9892b3647ea3367f.png" />`);
+const $racer1 = $(`<img class="one" id="tortoise" src="https://i.pinimg.com/originals/eb/39/35/eb3935ae7c66765699a16865187bcd6c.png" />`);
+const $racer2 = $(`<img class="two" id="hare" src="https://i.pinimg.com/originals/05/c0/68/05c068556d91081d9892b3647ea3367f.png" />`);
 const $instruction1 = $(`<p class="instruct1">Tortoise - Run by smashing "A"</p>`);
 const $instruction2 = $(`<p class="instruct2">Hare - Run by smashing "L"</p>`);
 const $timer = $(`<span id="timer">5</span>`);
@@ -28,6 +28,13 @@ const $timer = $(`<span id="timer">5</span>`);
     
 //       }, 1000);
 
+let tortBoundingRect = null;
+let tortBoundingRect = null;
+let boundingRect = document.getElementById('race-track').getBoundingClientRect();
+// console.log(boundingRect);
+let winX = boundingRect.width
+
+
 
 $('button').on('click', () => {
     //ADD ELEMENTS 
@@ -39,6 +46,10 @@ $('button').on('click', () => {
     $('.race-cars').append($racer1);
     $('.race-cars').append($racer2);
     
+    tortBoundingRect = document.getElementById('tortoise').getBoundingClientRect()
+    harBoundingRect = document.getElementById('hare').getBoundingClientRect()
+    console.log(tortBoundingRect);
+    console.log(harBoundingRect);
     //COUNTDOWN TIMER
     let seconds = document.getElementById("timerContainer").textContent;
     let countdown = setInterval(function() {
@@ -53,11 +64,12 @@ $('button').on('click', () => {
 // keycode 76	"L"
 let winnerOne = 0;
 let winnerTwo = 0;
+
+
 $(window).on("keydown", event => {
     if (event.keyCode === 65) {
-        
         $(".one").animate({ 
-            marginLeft: "+=2%",
+            marginLeft: "+=15px",
         }, 100 );
         winnerOne += 1;
         if (winnerOne === 45) {
@@ -67,7 +79,7 @@ $(window).on("keydown", event => {
     } else if (event.keyCode === 76) {
         
         $(".two").animate({ 
-            marginLeft: "+=2%",
+            marginLeft: "+=15px",
         }, 100 );
         winnerTwo += 1;
         if (winnerTwo === 45) {
