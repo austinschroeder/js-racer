2ba8804cfde84086ccbd4effde9bb06a7e4642e3
Austin Schroeder <austin@Andrews-iPhone.local>
2021-04-20 17:00:53 -0700

    Button adds elements as well as starts timer.  Everything functioning correctly

--- scripts/app.js
+++ scripts/app.js
@@ -1,49 +1,80 @@
 // When button is clicked:
 //      -2 race cars appear on left side of screen
 //      -instructions <p> appear on how to drive cars
+//      -Countdown timer appears and counts down
 //
 //Tap A to drive car 1 (move to the right)
 //Tap L to drive car 2 (move to the right)
 
 
 const $button = $('button');
-const $racer1 = $(`<div class="one" />`);
-const $racer2 = $(`<div class="two" />`);
-const $instruction1 = $(`<p class="instruct1">Racer 1-Drive by smashing "A"</p>`);
-const $instruction2 = $(`<p class="instruct2">Racer 2-Drive by smashing "L"</p>`);
+const $racer1 = $(`<img class="one" src="https://i.pinimg.com/originals/eb/39/35/eb3935ae7c66765699a16865187bcd6c.png" />`);
+const $racer2 = $(`<img class="two" src="https://i.pinimg.com/originals/05/c0/68/05c068556d91081d9892b3647ea3367f.png" />`);
+const $instruction1 = $(`<p class="instruct1">Tortoise - Run by smashing "A"</p>`);
+const $instruction2 = $(`<p class="instruct2">Hare - Run by smashing "L"</p>`);
+const $timer = $(`<span id="timer">10</span>`);
+
+
+
+// let timer = 3;
+// function startTimer() {
+//     const counter = setInterval(function() {
+//         timer--;
+//         console.log(timer)
+//         if (timer = 0) {
+            
+//             clearInterval(counter);
+//         }
+    
+//       }, 1000);
 
 
 $('button').on('click', () => {
+    //ADD ELEMENTS 
+    $('#timerContainer').append($timer);
+    
     $('.instructions').append($instruction1);
     $('.instructions').append($instruction2);
-
+    
     $('.race-cars').append($racer1);
     $('.race-cars').append($racer2);
-
+    
+    //COUNTDOWN TIMER
+    let seconds = document.getElementById("timerContainer").textContent;
+    let countdown = setInterval(function() {
+    seconds--;
+    document.getElementById("timer").textContent = seconds;
+    if (seconds <= 0) clearInterval(countdown);
+    }, 1000);
 });
 
 // RACING LOGIC
 // keycode 65	"A"	
 // keycode 76	"L"
-
 let winnerOne = 0;
 let winnerTwo = 0;
 $(window).on("keydown", event => {
     if (event.keyCode === 65) {
-       winnerOne += 1;
-       console.log(winner);
-
         
         $(".one").animate({ 
-            marginLeft: "+=10px",
-        }, 10 );
-
+            marginLeft: "+=20px",
+        }, 100 );
+        winnerOne += 1;
+        if (winnerOne === 38) {
+            window.alert('Tortoise has won the Race!');
+        }
+        
     } else if (event.keyCode === 76) {
-        winnerTwo += 1;
-       console.log(winner);
+        
         $(".two").animate({ 
-            marginLeft: "+=10px",
-        }, 10 );
+            marginLeft: "+=20px",
+        }, 100 );
+        winnerTwo += 1;
+        if (winnerTwo === 38) {
+            window.alert('Hare has won the Race!');
+        }
         
     }
 })
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
