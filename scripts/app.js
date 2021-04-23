0de8d8a7b31f2f81a7466be6d5ae8880a7a2fefc
Austin Schroeder <austin@Andrews-iPhone.local>
2021-04-22 18:27:55 -0700

    Added audio.  CSS alignment is looking good.  Need to create scoring system and stlye splash page.

--- scripts/app.js
+++ scripts/app.js
@@ -17,7 +17,13 @@ const $winner1 = $(`<p id="winner1"> THE TORTOISE IS VICTORIOUS </p>`)
 const $winner2 = $(`<p id="winner2"> THE HARE IS VICTORIOUS </p>`)
 const $go = $(`<p id="go"> GO! GO! GO! </p>`);
 const $reset = $(`<button id="reset-button">RESET</button>`)
-
+const $red1 = $(`<img class="lights" id="red1" src="https://lh3.googleusercontent.com/proxy/QrBSAkF98L3UwzR8ScvdnfHf6X_15UGoQOLbvSffCN9fKhfBiHjrLWlDwvEwgW68-SBlbfQJRhHsgue2hVdXGNZdfH5JOZGtFUFsC5ih5Whic7fQOaVAYvqCs_ba-es" />`)
+const $red2 = $(`<img class="lights" id="red2" src="https://lh3.googleusercontent.com/proxy/QrBSAkF98L3UwzR8ScvdnfHf6X_15UGoQOLbvSffCN9fKhfBiHjrLWlDwvEwgW68-SBlbfQJRhHsgue2hVdXGNZdfH5JOZGtFUFsC5ih5Whic7fQOaVAYvqCs_ba-es" />`)
+const $red3 = $(`<img class="lights" id="red3" src="https://lh3.googleusercontent.com/proxy/QrBSAkF98L3UwzR8ScvdnfHf6X_15UGoQOLbvSffCN9fKhfBiHjrLWlDwvEwgW68-SBlbfQJRhHsgue2hVdXGNZdfH5JOZGtFUFsC5ih5Whic7fQOaVAYvqCs_ba-es" />`)
+const $yellow1 = $(`<img class="lights" id="yellow1" src="https://lh3.googleusercontent.com/proxy/uvqUW44pblvmWD9we9AwpwW0E9koodadYCHFsrk-pxJQxU8XHNRVe6g-qSo5Xs-V9ZQXs8_TIwkxKcIpfsBypKTMKuTzm-cGSDbtEjHIUXdCLkNoT5F4Ww" />`)
+const $yellow2 = $(`<img class="lights" id="yellow2" src="https://lh3.googleusercontent.com/proxy/uvqUW44pblvmWD9we9AwpwW0E9koodadYCHFsrk-pxJQxU8XHNRVe6g-qSo5Xs-V9ZQXs8_TIwkxKcIpfsBypKTMKuTzm-cGSDbtEjHIUXdCLkNoT5F4Ww" />`)
+const $yellow3 = $(`<img class="lights" id="yellow3" src="https://lh3.googleusercontent.com/proxy/uvqUW44pblvmWD9we9AwpwW0E9koodadYCHFsrk-pxJQxU8XHNRVe6g-qSo5Xs-V9ZQXs8_TIwkxKcIpfsBypKTMKuTzm-cGSDbtEjHIUXdCLkNoT5F4Ww" />`)
+const $green = $(`<img class="lights" id="green" src="https://lh3.googleusercontent.com/proxy/HgjaH4AcBMcNAkB7J_A2UMG-hsKTWB9q-SzuUPRSlNA_a9dzb7pjHuokQ0bgUV9swOrND1WmsV5WuXx2rFMaRFjjhralYXUTbcxvsS0tTOs5cRjnDxgl7sDIpKg" />`)
 // let timer = 3;
 // function startTimer() {
 //     const counter = setInterval(function() {
@@ -36,15 +42,17 @@ const $reset = $(`<button id="reset-button">RESET</button>`)
 
 //START BUTTON
 $('.start-button').on('click', () => {
+    // Play Audio
+    //Crowd Cheers
+    const crowd = document.createElement("audio");
+    crowd.src = "./audio/crowd.mp3"; 
+    crowd.play(); 
+ 
+    //Countdown
     //ADD ELEMENTS 
-    $('#timerContainer').append($timer);
-    
-    $('#instruct-container1').append($instruction1);
-    $('#instruct-container2').append($instruction2);
-    
+    $('#timerContainer').append($timer);  
     $('.race-cars').append($racer1);
     $('.race-cars').append($racer2);
-    
     // tortBoundingRect = document.getElementById('tortoise').getBoundingClientRect()
     // harBoundingRect = document.getElementById('hare').getBoundingClientRect()
     // console.log(tortBoundingRect);
@@ -52,16 +60,39 @@ $('.start-button').on('click', () => {
     //COUNTDOWN TIMER
     let seconds = document.getElementById("timerContainer").textContent;
     let countdown = setInterval(function() {
-    seconds--;
-    document.getElementById("timer").textContent = seconds;
-    if (seconds <= 0) clearInterval(countdown);
-    if (seconds <= 0) {
-        $('.go-container').append($go);
-        $("#timerContainer").remove();
-    }
+        seconds--;
+        document.getElementById("timer").textContent = seconds;
+        if (seconds <= 0) clearInterval(countdown);
+        if (seconds <= 0) {
+            $('.go-container').append($go);
+            $("#timerContainer").remove();
+            $('#yellow1').remove();
+            $('#yellow2').remove();
+        }
+        if (seconds === 3) {
+            const countAudio = document.createElement("audio");
+            countAudio.src = "./audio/countdown2.mp3"; 
+            countAudio.play(); 
+            $(".placeholder").append($red1);
+            $(".placeholder").append($red2);
+            $(".placeholder").append($red3);
+        }
+        if (seconds === 2) {
+            $('#red1').remove();
+            $('#red2').remove();
+            $('#red3').remove();
+            $('.placeholder').append($yellow1);
+            $('.placeholder').append($yellow2);
+            $('.placeholder').append($yellow3);
+            
+        }
+        if (seconds === 1) {
+            $('#yellow3').remove();
+            $('.placeholder').append($yellow);
+            $('.placeholder').append($yellow);
+        }
     }, 1000);
 });
-
 
 // RACING LOGIC
 // keycode 65	"A"	
@@ -69,15 +100,16 @@ $('.start-button').on('click', () => {
 let winnerOne = 0;
 let winnerTwo = 0;
 
-
 $(window).on("keydown", event => {
     if (event.keyCode === 65) {
         $(".one").animate({ 
-            marginLeft: "+=2%",
+            left: "+=2%",
         }, 100 );
         winnerOne += 1;
         if (winnerOne === 45 && winnerTwo < 45) {
-            // window.alert('Tortoise has won the Race!');
+            const winnerMusic = document.createElement("audio");
+            winnerMusic.src = "./audio/victorymusic.mp3"; 
+            winnerMusic.play(); 
             $('#winner-container').append($winner1);
             $('#reset-container').append($reset)
             $(".go-container").remove();
@@ -86,11 +118,13 @@ $(window).on("keydown", event => {
     } else if (event.keyCode === 76) {
         
         $(".two").animate({ 
-            marginLeft: "+=2%",
+            left: "+=2%",
         }, 100 );
         winnerTwo += 1;
         if (winnerTwo === 45 && winnerOne < 45) {
-            // window.alert('Hare has won the Race!');
+            const winnerMusic = document.createElement("audio");
+            winnerMusic.src = "./audio/victorymusic.mp3"; 
+            winnerMusic.play(); 
             $('#winner-container').append($winner2);
             $('#reset-container').append($reset)
             $(".go-container").remove();
